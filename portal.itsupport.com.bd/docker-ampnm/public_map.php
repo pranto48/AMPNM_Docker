02629656af1117eb8c5749378dcde78fb47f2396
[dyad] <pranto48@gmail.com>
2025-11-09 09:53:34 +0600

    [dyad] wrote 2 file(s) + extra files edited outside of Dyad

--- portal.itsupport.com.bd/docker-ampnm/public_map.php
+++ portal.itsupport.com.bd/docker-ampnm/public_map.php
@@ -146,7 +146,7 @@ if ($map['background_image_url']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shared Map: <?= htmlspecialchars($map['name']) ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> <!-- Added Font Awesome -->
     <script src="https://unpkg.com/vis-network/standalone/umd/vis-network.min.js"></script>
     <link rel="stylesheet" href="assets/css/style.css">
     <style>
